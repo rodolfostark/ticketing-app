6636f5cd6c14449545be330597d8f6cc966cc92b
Jonatas Rodolfo Pereira dos Santos <rodolfojonatas@outlook.com>
2023-11-13 07:57:59 -0300

    chore: creating ticket type and update ts targer

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Ticket } from "@/types";
 import TicketCard from "./(components)/TicketCard";
 
 async function getTickets() {
@@ -13,19 +14,27 @@ async function getTickets() {
 
 
 export default async function Home() {
-  const { tickets } = await getTickets();
+  const tickets: Ticket[] = await getTickets();
   const uniqueCategories = [
-    ... new Set(tickets?.map(({category}) => category)),
-  ]
+    ... new Set(tickets?.map(({ category }) => category)),
+  ];
 
   return (
     <div className="p-5">
-      <div className="lg:grid grid-cols-2 xl:grid-cols-4">
-        <TicketCard />
-        <TicketCard />
-        <TicketCard />
-        <TicketCard />
-        <TicketCard />
+      <div>
+        {
+          tickets && uniqueCategories?.map((uniqueCategory, categoryIndex) => (
+            <div key={categoryIndex} className="mb-4">
+              <h2>{uniqueCategory}</h2>
+              <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+                {tickets.filter((ticket) => ticket.category === uniqueCategory).map(
+                  (filteredTicket, _index) => (
+                    <TicketCard id={_index} key={_index} ticket={ticket} />
+                  )
+                )}
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   )
---
+++ types/index.ts
@@ -0,0 +1,9 @@
+export type Ticket = {
+    title: string,
+    description: string,
+    category: string,
+    priority: number,
+    progress: number,
+    status: string,
+    active: boolean
+}
